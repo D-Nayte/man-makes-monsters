ed75eca6d8751f5c90e73ca8ef810e91d0f8d2aa
Andy Schunke <schunke.andy@gmail.com>
2023-02-03 14:35:26 +0100

    change socket conecction link

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -7,7 +7,7 @@ import { useEffect, useState } from "react";
 import { parseCookies, setCookie } from "nookies";
 import { ContextWrapper } from "../context";
 
-export const socket = io("http://localhost:5555", {
+export const socket = io("https://man-makes-monsters-servr.onrender.com", {
   reconnection: true, // enable reconnection
   reconnectionAttempts: 5, // try to reconnect 5 times
   reconnectionDelay: 3000, // increase the delay between reconnection attempts to 3 seconds
