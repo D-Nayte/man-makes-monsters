7ad3b4dedd774ec7371faab9ffb361ac4621e890
Danni Malka <dannimalka.iag@gmail.com>
2023-02-03 20:40:20 +0200

    fixed styling profil for mobile

--- client/pages/api/auth/[...nextauth].js
+++ client/pages/api/auth/[...nextauth].js
@@ -1,14 +1,11 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-export const authOptions = {
-  // Configure one or more authentication providers
+export default NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
-
-    // ...add more providers here
   ],
-};
-export default NextAuth(authOptions);
+  secret: process.env.NEXTAUTH_SECRET,
+});
