bb13bd4c69e791b5185cd3556960623f5abd52c1
Andy Schunke <schunke.andy@gmail.com>
2023-02-03 19:27:19 +0100

    prefix socket.io after server reconnect

--- client/pages/api/auth/[...nextauth].js
+++ client/pages/api/auth/[...nextauth].js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
@@ -7,8 +8,6 @@ export const authOptions = {
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
-
-    // ...add more providers here
   ],
 };
 export default NextAuth(authOptions);
