bd82718e47154dff92a65a9c67e8d703fe58bdec
Andy Schunke <schunke.andy@gmail.com>
2023-02-04 08:58:01 +0100

    add env for cokse.io

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -7,12 +7,14 @@ import { useEffect, useState } from "react";
 import { parseCookies, setCookie } from "nookies";
 import { ContextWrapper } from "../context";
 
-export const socket = io("https://man-makes-monsters-servr.onrender.com", {
-  // http://localhost:5555
-  reconnection: true, // enable reconnection
-  reconnectionAttempts: 5, // try to reconnect 5 times
-  reconnectionDelay: 3000, // increase the delay between reconnection attempts to 3 seconds
-});
+export const socket = io(
+  process.env.NEXT_SERVERADDRESS || "http://localhost:5555",
+  {
+    reconnection: true, // enable reconnection
+    reconnectionAttempts: 5, // try to reconnect 5 times
+    reconnectionDelay: 3000, // increase the delay between reconnection attempts to 3 seconds
+  }
+);
 
 function MyApp({ Component, router, pageProps: { session, ...pageProps } }) {
   const cookies = parseCookies();
@@ -55,8 +57,7 @@ function MyApp({ Component, router, pageProps: { session, ...pageProps } }) {
           handSize={handSize}
           amountOfRounds={amountOfRounds}
           language={language}
-          setLanguage={setLanguage}
-        >
+          setLanguage={setLanguage}>
           <AnimatePresence mode="wait" initial={false}>
             <Component
               key={router.pathname}
