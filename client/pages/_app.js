fbd985e8be8ec9050f7d102c7fd26eb86962242f
Andy Schunke <schunke.andy@gmail.com>
2023-02-04 11:12:21 +0100

    test env

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -7,14 +7,11 @@ import { useEffect, useState } from "react";
 import { parseCookies, setCookie } from "nookies";
 import { ContextWrapper } from "../context";
 
-export const socket = io(
-  process.env.NEXT_SERVERADDRESS || "http://localhost:5555",
-  {
-    reconnection: true, // enable reconnection
-    reconnectionAttempts: 5, // try to reconnect 5 times
-    reconnectionDelay: 3000, // increase the delay between reconnection attempts to 3 seconds
-  }
-);
+export const socket = io(process.env.NEXT_SERVERADDRESS, {
+  reconnection: true, // enable reconnection
+  reconnectionAttempts: 5, // try to reconnect 5 times
+  reconnectionDelay: 3000, // increase the delay between reconnection attempts to 3 seconds
+});
 
 function MyApp({ Component, router, pageProps: { session, ...pageProps } }) {
   const cookies = parseCookies();
